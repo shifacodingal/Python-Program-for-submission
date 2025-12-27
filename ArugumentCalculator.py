8f3b96b4c655d9eae94562fbaee13b1f4e04acbe
shifacodingal <shifacodingal@gmail.com>
2025-12-27 22:48:32 +0530

    Calculator with Argument

---
+++ ArugumentCalculator.py
@@ -0,0 +1,24 @@
+# Function definitions
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
+# Taking input
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Addition:", add(x, y))
+print("Subtraction:", subtract(x, y))
+print("Multiplication:", multiply(x, y))
+print("Division:", divide(x, y))
