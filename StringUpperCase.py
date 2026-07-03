602c3f943654e51391e64bc2c7740299fc4aef89
shifacodingal <shifacodingal@gmail.com>
2026-07-03 15:29:18 +0530

    M-21 String Upper Case

---
+++ StringUpperCase.py
@@ -0,0 +1,15 @@
+
+class IOString():
+
+	# constructor to set default value
+    def __init__(self):
+        self.str1 = ""
+    def get_String(self):
+        self.str1 = input("Enter String : ")
+    def print_String(self):
+        print("Result is :", self.str1.upper())
+
+str1 = IOString()
+
+str1.get_String()
+str1.print_String()
